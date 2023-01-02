72c92bf2f8b8abb9fde2013d9f5ead33f7b79fe2
Kevin Mollencopf <kevin4463@godaddy.com>
2023-01-02 15:03:59 -0700

    wip

---
+++ ExampleMvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using cqrsCore;
+using ExampleMvc.CommandContracts;
+using Microsoft.AspNetCore.Mvc;
+using ExampleMvc.Models;
+
+namespace ExampleMvc.Controllers;
+
+public class HomeController : Controller
+{
+  private readonly ILogger<HomeController> _logger;
+  private readonly ICqrsManager _cqrsManager;
+
+  public HomeController(ILogger<HomeController> logger, ICqrsManager cqrsManager)
+  {
+    _logger = logger;
+    _cqrsManager = cqrsManager;
+  }
+
+  public IActionResult Index()
+  {
+    return View();
+  }
+
+  public IActionResult Privacy()
+  {
+    return View();
+  }
+
+  [HttpGet]
+  public async Task<IActionResult> TestCqrs()
+  {
+    await _cqrsManager.ExecuteAsync(new ExampleCommand
+    {
+      ExampleName = "MRBeast",
+      TimeStamp = DateTime.Now,
+      ExecuteAsNoOp = false,
+      ContextData = new Dictionary<string, object>()
+    }, CancellationToken.None);
+
+    return Ok();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+  }
+}
---
+++ ExampleMvc/Program.cs
@@ -0,0 +1,70 @@
+
+using System.Reflection;
+using cqrsCore.Decorators.Command;
+using cqrsCore.Extensions;
+using cqrsCore.Validation;
+using ExampleMvc.CommandHandlers;
+using Serilog;
+using SimpleInjector;
+
+var container = new Container();
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+Log.Logger = new LoggerConfiguration()
+  .MinimumLevel.Verbose()
+  .Enrich.FromLogContext()
+  .CreateLogger();
+
+// Add services to the container.
+services.AddControllersWithViews();
+services.AddLogging(b => b.AddSerilog());
+services.AddSimpleInjector(container, opt =>
+{
+  opt.AddAspNetCore();
+  opt.AddLogging();
+});
+
+container.AddCqrs()
+  .AddCommandHandlers(GetCommandAssemblies())
+  .DecorateWith(typeof(AsyncCommandHandlerDecorator<>))
+  .And()
+  .WithCqrsValidation<DataAnnotationValidator>()
+  .Build();
+
+var app = builder.Build();
+
+app.Services.UseSimpleInjector(container);
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+  name: "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}");
+
+container.Verify();
+
+app.Run();
+
+// Helpers
+List<Assembly> GetCommandAssemblies()
+{
+  List<Assembly> assemblies = new List<Assembly>();
+  
+  assemblies.Add(typeof(ExampleCommandHandler).Assembly);
+
+  return assemblies;
+}
